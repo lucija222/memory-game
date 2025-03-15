85625686af631534744f6853096f0966c8664d3c
Lucija <lucia.hranjec@gmail.com>
2025-03-15 20:15:58 +0100

    Connected svgs to grid

---
+++ script.js
@@ -0,0 +1,5 @@
+const memoryGrid = document.querySelector("#memory-grid");
+const gridImages = document.querySelectorAll("#memory-grid > img");
+const resetBtn = document.querySelector("#reset-btn");
+
+const numOfTurnedCards = 0; //0, 1 || 2
