39fdf5d1e20562692117e7c059094889a3a764a1
Lucija <lucia.hranjec@gmail.com>
2025-03-20 18:32:22 +0100

    Added card/icons randomization

--- modules/functions.js
+++ modules/functions.js
@@ -1,7 +1,9 @@
 let flippedCards = { firstCard: [], secondCard: [] }; //[contentElem, cardIcon]
 let numOfFlippedCards = 0; //0, 1 || 2
-let numOfPairs = 8;
 let numOfMatches = 0;
+const cardImgElements = document.querySelectorAll(
+    "#memory-grid .back-side img"
+);
 
 const checkCardsForMatch = () => {
     const cardIcon1 = flippedCards.firstCard[1];
@@ -46,7 +48,38 @@ const flipCard = (e) => {
     }
 };
 
-const resetGame = () => {
+const updateCardImages = (array) => {
+    cardImgElements.forEach((img, index) => {
+        const alt = array[index];
+        img.alt = alt;
+        img.src = `./icons/${alt}.svg`;
+    });
+};
+
+const getRandomIconPairs = (array, numOfPairs) => {
+    const shuffledArr = shuffleArray([...array]);
+    const extractedArr = shuffledArr.slice(0, numOfPairs);
+    return [...extractedArr, ...extractedArr];
+};
+
+const shuffleArray = (array) => {
+    const newArr = [...array];
+    for (let i = newArr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+    }
+
+    return newArr;
+};
+
+const randomizeIconsAndShuffleCards = (array, numOfPairs) => {
+    const iconsArr = getRandomIconPairs(array, numOfPairs);
+    const shuffledArr = shuffleArray(iconsArr);
+    updateCardImages(shuffledArr);
+};
+
+const resetGame = (e, array, numOfPairs) => {
+    e.stopPropagation();
     const cards = document.querySelectorAll(".card .content");
 
     cards.forEach((card) => {
@@ -57,8 +90,8 @@ const resetGame = () => {
 
     numOfFlippedCards = 0;
     numOfMatches = 0;
+    randomizeIconsAndShuffleCards(array, numOfPairs);
     console.log("GAME RESET");
-    
 };
 
-export { flipCard, resetGame,  };
+export { flipCard, resetGame, randomizeIconsAndShuffleCards };
--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 "use-strict";
-import { flipCard, resetGame } from "./modules/functions.js";
+import { flipCard, randomizeIconsAndShuffleCards, resetGame } from "./modules/functions.js";
 
-const memoryGrid = document.querySelector("#memory-grid");
+// const memoryGrid = document.querySelector("#memory-grid");
 const cards = document.querySelectorAll(".card");
-// const cardsContent = document.querySelectorAll(".card .content");
 const resetBtn = document.querySelector("#reset-btn");
 
+// const icons = ["bird", "bush", "cactus1", "deer", "fox", "leaf1", "leaves1", "tree1"];
+let numOfPairs = 8;
+const icons = ["bird", "bush", "cactus1", "cactus2", "cactus3", "deer", "flamingo", "fox", "leaf1", "leaf2", "leaves1", "leaves2", "mountain", "tree1", "tree2"];
+
+document.addEventListener("DOMContentLoaded", () => {
+    randomizeIconsAndShuffleCards(icons, numOfPairs);
+});
 
 cards.forEach((card) => {
     card.addEventListener("click", flipCard);
 }); 
 
-resetBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", (e) => {
+    resetGame(e, icons, numOfPairs);
+});
